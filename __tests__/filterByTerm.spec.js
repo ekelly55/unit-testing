d183bd6dfa9616d4c8abb53a28ac3379af388f8c
ekelly55 <eamonn.michael.kellyr@email.com>
2022-12-02 14:39:12 -0500

    added coverage and html reports

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,12 +1,5 @@
 //spec is a convention borrowed from Ruby for marking the file as the specification for a given functionality
-
-
-function filterByTerm(inputArr, searchTerm) {
-    const regex = new RegExp(searchTerm, "i");
-    return inputArr.filter(function(arrayElement) {
-      return arrayElement.url.match(regex);
-    });
-  }
+const filterByTerm = require("../src/filterByTerm");
 
 //describe is a wrapper for related tests. it contains two parameters: a string for descritbing the test suite and a callback function for wrappign the test
 describe("Filter function", () => {
@@ -31,15 +24,17 @@ describe("Filter function", () => {
             expect(filterByTerm(input, "url")).toEqual(output);
             
         });
-        test("it should filter by a search term ()", () => {
+        test("it should filter by a search term ( )", () => {
             const input = [
                 { id: 1, url: "https://www.url1.dev" },
                 { id: 2, url: "https://www.url2.dev" },
                 { id: 3, url: "https://www.link3.dev" }
             ];
             const output = [];
-            expect(filterByTerm(input, null)).toEqual(output);
+            expect(filterByTerm(input, " ")).toEqual(output);
             
         });
     });
     // expect(filterByTerm(input, " ")).toEqual(output);
+
+    //added coverage...it tells you what isn't covered by your tests so you know what to add?
